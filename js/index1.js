147d2f6a9bafca2b6b8a4498a70bfabf80524536
unknown <2daw3@HZ114496.H012108.NET>
2023-01-16 10:17:43 +0100

    drag and drop solved

--- js/index1.js
+++ js/index1.js
@@ -1,54 +1,7 @@
-// var vInicio = new Array;
-// var vFin = new Array;
-
-// //0 significa visible y 1 invisible
-
-// for(let x = 0; x < 10; x++){
-//     vInicio [x] = 0;
-//     vFin [x] = 1;
-// }
+var guardados = new Array;
 
 $(function () {
-    // var inicio = $("#inicio");
-    // var fin = $("#fin");
-    
-
-    // $("img", inicio).draggable({
-    //     cancel: "a.ui-icon",
-    //     rever: "invalid",
-    //     containment: "document",
-    //     helper:"clone",
-    //     cursorAt: { bottom: 0 },
-    //     drag: function(event, ui){
-            
-    //         // console.log($(this).attr("id"));
-    //         info = $(this).attr("id");
-    //         vInicio[info] = 1;
-    //         console.log(vInicio);
-    //         // console.log(info);
-    //         $(this).css('visibility', 'hidden');
-            
-    //     }
-    // }); 
-    
-    // $(fin).droppable({
-    //     accept: "#inicio > img",
-    //     classes: {
-    //         "ui-droppable-active": "ui-state-highlight"
-    //     },
-    //     drop: function(event, ui){
-    //         // $(info).css('visibility', 'visible');
-    //         console.log(info);
-    //         var cambio;
-    //         // document.getElementById(info) = cambio;
-
-    //         // cambio.css('visibility', 'visible');
-
-    //         vFin[info] = 0;
-    //         console.log(vFin);
-    //     }
-    // })
-
+ 
     $("#inicio img").draggable({
         revert: "invalid",
         refreshPositions: true,
@@ -66,21 +19,32 @@ $(function () {
             }
         }
     });
+    var x=0;
     $("#fin").droppable({
         drop: function (event, ui) {
             if ($("#fin img").length == 0) {
                 $("#fin").html("");
             }
+            console.log(ui.draggable.attr('id'));
             ui.draggable.addClass("dropped");
             $("#fin").append(ui.draggable);
+            
+            guardados[x] = ui.draggable.attr('id');
+            x++;
         }
     });
 
-
-
-
-
 });
+
+
+function guardado(){
+    console.log(guardados);
+
+}
+
+function borrado(){
+
+}
 
 // https://stackoverflow.com/questions/27263460/using-jquery-to-drag-and-drop-get-value-from-dropped-div
 
