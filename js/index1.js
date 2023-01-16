e4dbb87cbf35461175541f253027f5185fdb1eb0
unknown <2daw3@HZ114496.H012108.NET>
2023-01-16 14:22:51 +0100

    chart

--- js/index1.js
+++ js/index1.js
@@ -77,19 +77,19 @@ function guardado(){
     }
     
 }
-
-const labels = Utils.months({count: 7});
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First Dataset',
-    data: [65, 59, 80, 81, 56, 55, 40],
-    fill: false,
-    borderColor: 'rgb(75, 192, 192)',
-    tension: 0.1
-  }]
-};
-
+function grafico(){
+    const labels = Utils.months({count: 7});
+    const data = {
+    labels: labels,
+    datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+    }]
+    };
+}
 // https://stackoverflow.com/questions/27263460/using-jquery-to-drag-and-drop-get-value-from-dropped-div
 
 // https://jqueryui.com/draggable/#events
