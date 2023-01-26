d319bececcc0eb53b9b2d0ab94100d9a794bf9a2
unknown <2daw3@HZ114496.H012108.NET>
2023-01-26 12:44:50 +0100

    data changes

--- js/index1.js
+++ js/index1.js
@@ -1,7 +1,7 @@
 var guardados = new Array;
 
 var show = new Array;
-    show[0] =  '<img id="0" src="img/bbva.png" alt="BBVA" onclick="grafico()">';
+    show[0] =  '<img id="0" src="img/bbva.png" alt="BBVA" onclick="eleccion(0)">';
     show[1] =  '<img id="1" src="img/caixa.png" alt="Caixa" onclick="eleccion(1)">';
     show[2] =  '<img id="2" src="img/cellnex.png" alt="Cellnex" onclick="eleccion(2)">';
     show[3] =  '<img id="3" src="img/ferrovial.png" alt="Ferrovial" onclick="eleccion(3)">';
@@ -12,7 +12,7 @@ var show = new Array;
     show[8] =  '<img id="8" src="img/santander.png" alt="Santander" onclick="eleccion(8)">';
     show[9] =  '<img id="9" src="img/telefonica.png" alt="Telefonica" onclick="eleccion(9)">';
 
-var imagesId = [];
+var imagesId = []; //localStorage
 var storedImagesId = JSON.parse(localStorage.getItem("imagesId"));
 
 $(function () {
@@ -27,12 +27,6 @@ $(function () {
             ui.helper.removeClass("draggable");
             var image = this.src.split("/")[this.src.split("/").length - 1];
 
-            // if ($.ui.ddmanager.drop(ui.helper.data("draggable"), event)) {
-            //     alert(image + " dropped.");
-            // }
-            // else {
-            //     alert(image + " not dropped.");
-            // }
         }
     });
 
@@ -56,20 +50,55 @@ $(function () {
         }
     });
 
-    $('#borrar').click(function(){
-        location.reload();
+    ////////////
+
+    $("#inicio img").droppable({
+        revert: "invalid",
+        refreshPositions: true,
+        drop: function (event, ui) {
+            ui.helper.addClass("draggable");
+        },
+        stop: function (event, ui) {
+            ui.helper.removeClass("draggable");
+            var image = this.src.split("/")[this.src.split("/").length - 1];
+
+        }
     });
+
+    var x=0;
+
+    $("#fin").draggable({
+        drag: function (event, ui) {
+            if ($("#fin img").length == 0) {
+                $("#fin").html("");
+            }
+            // console.log(ui.draggable.attr('id'));
+            ui.draggable.addClass("dropped");
+            $("#fin").append(ui.draggable);
+            
+            guardados[x] = ui.draggable.attr('id');
+            imagesId.push(ui.draggable.attr('id'));
+
+            localStorage.setItem("imagesId", JSON.stringify(imagesId));
+            x++;
+            guardado();
+        }
+    });
+
+    // $('#borrar').click(function(){
+    //     location.reload();
+    // });
 
 });
 
 
-function borrar(){
-    $(function (){
-        $('#borrar').click(function(){
-            location.reload();
-        });
-    });
-}
+// function borrar(){
+//     $(function (){
+//         $('#borrar').click(function(){
+//             location.reload();
+//         });
+//     });
+// }
 
 function guardado(){
     console.log(guardados);
@@ -83,11 +112,13 @@ function guardado(){
             toShow += `<div id='div${guardados[x]}'>`;
             toShow += show[guardados[x]] + `<p id='valor${guardados[x]}'>Valor actual</p>`;
             // toShow += '<canvas id="myChart"></canvas>';
+            toShow += `</div>` + "<br>";
         }else{
+            toShow += `<div id='div${guardados[x]}'>`;
             toShow += show[guardados[x]] + `<p id='valor${guardados[x]}'>Valor actual</p>`;
-            
+            toShow += `</div>`;
         }
-        toShow += `</div>`;
+        
     }
 
     mostrar.innerHTML = toShow;
@@ -114,87 +145,64 @@ function getValor(){
 }
 
 
-
-function grafico(){
-        var ctx = document.getElementById("myChart").getContext("2d");
-        var myChart = new Chart(ctx, {
-            type: "bar",
-            data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: "# of Votes",
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        "rgba(255, 99, 132, 0.2)",
-                        "rgba(54, 162, 235, 0.2)",
-                        "rgba(255, 206, 86, 0.2)",
-                        "rgba(75, 192, 192, 0.2)",
-                        "rgba(153, 102, 255, 0.2)",
-                        "rgba(255, 159, 64, 0.2)"
-                    ],
-                    borderColor: [
-                        "rgba(255, 99, 132, 1)",
-                        "rgba(54, 162, 235, 1)",
-                        "rgba(255, 206, 86, 1)",
-                        "rgba(75, 192, 192, 1)",
-                        "rgba(153, 102, 255, 1)",
-                        "rgba(255, 159, 64, 1)"
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    
-    // const labels = Utils.months({count: 7});
-    // const data = {
-    // labels: labels,
-    // datasets: [{
-    //     label: 'My First Dataset',
-    //     data: [65, 59, 80, 81, 56, 55, 40],
-    //     fill: false,
-    //     borderColor: 'rgb(75, 192, 192)',
-    //     tension: 0.1
-    // }]
-    // };
-
-//     var ctx = document.getElementById('myChart').getContext('2d');
-// var chart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         data: {
-//     labels: ['Enero', 'Febrero', 'Marzo', 'Abril'],
-//     datasets: [{
-//         label: 'Ventas',
-//         data: [12, 19, 3, 5],
-//         backgroundColor: 'rgba(255, 99, 132, 0.2)',
-//     }]
-// },
-
-//     },
-//     options: {
-//     scales: {
-//         y: {
-//             beginAtZero: true
-//         }
-//     },
-//     title: {
-//         display: true,
-//         text: 'Ventas mensuales'
-//     }
+// function grafico(){
+//         var ctx = document.getElementById("myChart").getContext("2d");
+//         var myChart = new Chart(ctx, {
+//             type: "bar",
+//             data: {
+//                 labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//                 datasets: [{
+//                     label: "# of Votes",
+//                     data: [12, 19, 3, 5, 2, 3],
+//                     backgroundColor: [
+//                         "rgba(255, 99, 132, 0.2)",
+//                         "rgba(54, 162, 235, 0.2)",
+//                         "rgba(255, 206, 86, 0.2)",
+//                         "rgba(75, 192, 192, 0.2)",
+//                         "rgba(153, 102, 255, 0.2)",
+//                         "rgba(255, 159, 64, 0.2)"
+//                     ],
+//                     borderColor: [
+//                         "rgba(255, 99, 132, 1)",
+//                         "rgba(54, 162, 235, 1)",
+//                         "rgba(255, 206, 86, 1)",
+//                         "rgba(75, 192, 192, 1)",
+//                         "rgba(153, 102, 255, 1)",
+//                         "rgba(255, 159, 64, 1)"
+//                     ],
+//                     borderWidth: 1
+//                 }]
+//             },
+//             options: {
+//                 scales: {
+//                     y: {
+//                         beginAtZero: true
+//                     }
+//                 }
+//             }
+//         });
 // }
 
-// });
+
+function eleccion(n){
+    alert(n);
 
 }
 
-
-function eleccion(){
-    alert("hola");
+function existeEnArray(numero, guardados) {
+    var ToF = false;
+    for(let x=0; x< guardados.length; x++){
+        if(guardados[x]==numero){
+            ToF = true;
+        }
+    }
+    return ToF;
 }
+
+function remover(num)
+{
+    var resultado = existeEnArray(num, guardados);
+
+    console.log(resultado);
+}
+
